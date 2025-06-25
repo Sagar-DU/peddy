9c28ee3d59de9c3c375af21965dde9395d929044
Sagar-DU <nahidhasansagar@gmail.com>
2025-06-26 04:27:26 +0600

    pet integration done with fetch

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,11 @@ console.log("Started working with API");
 
 // Load all the pet categories
 const loadPetCategories = () => {
-    fetch ("https://openapi.programming-hero.com/api/peddy/categories")
-    .then ((res) => res.json())
-    .then ((dat) => displayPetCateogries(dat.categories))
+    fetch("https://openapi.programming-hero.com/api/peddy/categories")
+        .then((res) => res.json())
+        .then((dat) => displayPetCateogries(dat.categories))
 
-    .catch((err) => console.log(err));
+        .catch((err) => console.log(err));
 }
 
 // Create a display pet categories function 
@@ -14,7 +14,7 @@ const displayPetCateogries = (categories) => {
     const petCateogryContainer = document.getElementById("pet-categories");
     // console.log(categories);
     categories.forEach((category) => {
-        console.log(category);
+        // console.log(category);
         const button = document.createElement("button");
         button.classList = "btn btn-wide h-22 bg-white rounded-2xl lato-bold text-2xl";
         button.innerHTML = `<img src="${category.category_icon}" alt="category_icon"> ${category.category}`;
@@ -22,6 +22,44 @@ const displayPetCateogries = (categories) => {
     });
 }
 
+// Load all the pets 
+const loadPets = () => {
+    fetch("https://openapi.programming-hero.com/api/peddy/pets")
+        .then((res) => res.json())
+        .then((dat) => displayPets(dat.pets))
+
+        .catch((err) => console.log(err));
+}
+
+// Display pets 
+const displayPets = (pets) => {
+    const allPetData = document.getElementById("pet-cards");
+    pets.forEach((pet) => {
+        console.log(pet);
+        const card = document.createElement("div");
+        card.classList = "card bg-base-100 shadow-sm"
+        card.innerHTML = `
+        <figure>
+    <img
+      src=${pet.image}
+      alt=${pet.cetagory} />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">
+      Card Title
+      <div class="badge badge-secondary">NEW</div>
+    </h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <div class="badge badge-outline">Fashion</div>
+      <div class="badge badge-outline">Products</div>
+    </div>
+  </div>
+        `;
+        allPetData.append(card);
+    })
+
+}
 /*
 Output
 category
@@ -34,4 +72,8 @@ id
 : 
 1
 */
+// Load Pet Categories 
 loadPetCategories();
+
+// Load All the pets 
+loadPets();
